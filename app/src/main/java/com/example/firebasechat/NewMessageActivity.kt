cbfa8c366151dd6109e5ad182cfef9ed9242249a
Daron Short <daron.short@hotmail.com>
2019-03-17 12:15:08 -0500

    Finished Recycleview for Users.

--- app/src/main/java/com/example/firebasechat/NewMessageActivity.kt
+++ app/src/main/java/com/example/firebasechat/NewMessageActivity.kt
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.Group
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row.view.*
 
 class NewMessageActivity : AppCompatActivity() {
 
@@ -16,15 +24,37 @@ class NewMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_message)
 
         supportActionBar?.title = "Select User"
+        fetchUsers()
+    }
 
-        val adapter = GroupAdapter<ViewHolder>()
-        adapter.add(UserItem())
-        newMessage_recycle_view.adapter = adapter
+    private fun fetchUsers() {
+
+       val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+                p0.children.forEach{
+                   val user = it.getValue(User::class.java)
+                    if(user != null){
+                        adapter.add(UserItem(user))
+                    }
+
+                }
+                newMessage_recycle_view.adapter = adapter
+            }
+
+        })
     }
 }
 
-class UserItem: Item<ViewHolder>() {
+class UserItem(val user: User): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.username_text.text = user.username
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.user_profile_image)
 
     }
     override fun getLayout(): Int {
--- app/src/main/java/com/example/firebasechat/RegisterActivity.kt
+++ app/src/main/java/com/example/firebasechat/RegisterActivity.kt
@@ -101,4 +101,6 @@ class RegisterActivity : AppCompatActivity() {
 
 }
 
-class User(val uid: String, val username: String, val profileImageUrl: String)
+class User(val uid: String, val username: String, val profileImageUrl: String) {
+    constructor() : this("","","")
+}
